3bd84db5282565592fc930436c0f9b2ab9007b92
Cartomu Kabba <cartomukabba123@gmail.com>
2019-04-16 21:45:56 +0000

    4/16

--- js/script.js
+++ js/script.js
@@ -4,8 +4,19 @@
 /* global $ */
 
 $("#search-button").click(function(){
+  var search_term = $("#search-term").val();
   
-  
+    $.ajax({
+        url: "https://api.giphy.com/v1/gifs/search?q=" +search_term+ "&rating=pg&api_key=dc6zaTOxFJmzC",
+        method: "GET",
+        success: function(response) {
+            var random = Math.floor(Math.random()*10);
+            $(".text-center").empty()
+           $(".text-center").append("<img src=" + response.data[random].images.original.url + ">");
+             
+            
+        },
+    });
   
 });
 
